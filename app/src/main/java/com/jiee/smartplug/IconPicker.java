c336af2bc2913c3cf4bcf3191a8217b1ca7bdb6e
rgbetanco <rgbetanco@hotmail.com>
2016-06-05 13:06:54 +0800

    May 2nd, 2016

--- app/src/main/java/com/jiee/smartplug/IconPicker.java
+++ app/src/main/java/com/jiee/smartplug/IconPicker.java
@@ -6,6 +6,13 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.BitmapShader;
+import android.graphics.Canvas;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.RectF;
+import android.graphics.Shader;
 import android.media.Image;
 import android.net.Uri;
 import android.os.AsyncTask;
@@ -31,6 +38,12 @@ import java.io.IOException;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 
+/*
+
+    Designed by Chinsoft Company, 2016. Copyrighted
+
+ */
+
 public class IconPicker extends Activity {
 
     GridView list;
@@ -38,6 +51,16 @@ public class IconPicker extends Activity {
     ImageButton btn_camera;
     static final int REQUEST_IMAGE_CAPTURE = 1;
     String callerActivity = null;
+
+    public final static int DECODE_FLAG_NONE    = 0x0000; // return as is
+    public final static int DECODE_FLAG_CROP    = 0x0001; // crop image to fit within bounds
+    public final static int DECODE_FLAG_FIT     = 0x0002; // fit within given bounds
+    public final static int DECODE_FLAG_CROPFIT  = 0x0003; // crop and fit image
+    // combine CROP | FIT to shrink image so that smallest dimension fits, and then crop the image to fit
+    public final static int DECODE_FLAG_ROUNDEDGE= 0x0004; // round corners
+
+    public final static int DECODE_FLAG_SCALEDOWN= 0x0008; // scale down
+    public final static int DECODE_FLAG_SCALE_P2 = 0x0010; // round to power of 2
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -73,10 +96,101 @@ public class IconPicker extends Activity {
 
     }
 
+    public static Rect calculateBitmapCropSrcRect( int srcW, int srcH, int dstW, int dstH ) {
+        final float srcAspect = (float)srcW / (float)srcH;
+        final float dstAspect = (float)dstW / (float)dstH;
+
+        if( srcAspect > dstAspect ) {
+            final int finalW = (int)(srcH * dstAspect);
+            final int l = (srcW - finalW) / 2;
+            return new Rect( l, 0, l + finalW, srcH );
+        } else {
+            final int finalH = (int)(srcW / dstAspect);
+            final int t = (srcH - finalH) / 2;
+            return new Rect( 0, t, srcW, t + finalH );
+        }
+    }
+
+    public static Bitmap cropBitmap( Bitmap bitmap, int w, int h, int flags ) {
+        final int srcW = bitmap.getWidth();
+        final int srcH = bitmap.getHeight();
+
+        // if scaleToFit == false, crop out w,h from bitmap
+        float scale = 1;
+        if( srcW < w )
+            scale = (float)srcW / w;
+
+        if( srcH < h ) {
+            float scaleH = (float)srcH / h;
+            if( scaleH < scale )
+                scale = scaleH;
+        }
+
+        if( scale < 1 ) {
+            w *= scale;
+            h *= scale;
+        }
+
+        Rect  srcRect;
+        RectF dstRect = new RectF(0,0,w,h);
+
+        if( (flags & DECODE_FLAG_FIT) == DECODE_FLAG_FIT ) {
+            srcRect = calculateBitmapCropSrcRect( srcW, srcH, w, h );
+        } else {
+            // crop image to center
+            int l = (srcW - w) / 2;
+            int t = (srcH - h) / 2;
+
+            srcRect = new Rect(l,t,w+l,h+t);
+        }
+
+        Bitmap scaledBitmap = null;
+
+        if( (flags & DECODE_FLAG_ROUNDEDGE)==DECODE_FLAG_ROUNDEDGE ) {
+            // create a new bitmap with rounded edges
+            scaledBitmap = Bitmap.createBitmap( (int)dstRect.width(), (int)dstRect.height(), Bitmap.Config.ARGB_8888 );
+            Canvas canvas = new Canvas(scaledBitmap);
+
+            BitmapShader shader = new BitmapShader(bitmap, Shader.TileMode.CLAMP, Shader.TileMode.CLAMP);
+            Matrix m = new Matrix();
+            m.setRectToRect(new RectF(srcRect), dstRect, Matrix.ScaleToFit.FILL);
+            shader.setLocalMatrix(m);
+
+            Paint paint = new Paint();
+            paint.setAntiAlias(true);
+            paint.setShader(shader);
+
+            float radius = Math.max(w,h) * 0.10f;
+
+            canvas.drawRoundRect(dstRect, radius, radius, paint);
+        } else {
+            // crop without rounding the edges... we could just crop, unless we're also scaling down (in which case, we
+            // scale down to save memory)
+            if( srcRect.width()==0 )
+                return null;   // error.
+
+            // crop the source rectangle, while applying a matrix to resize it to the proper size
+            scale = dstRect.width() / srcRect.width();
+
+            Matrix m = null;
+            if( scale<1 ) {
+                m = new Matrix();
+                m.postScale(scale, scale);
+            }
+            scaledBitmap = Bitmap.createBitmap(bitmap, srcRect.left, srcRect.top, srcRect.width(), srcRect.height(), m, true);
+        }
+
+        return scaledBitmap;
+    }
+
     public Boolean savebitmap(Bitmap bmp) throws IOException {
+
         boolean toReturn = false;
         ByteArrayOutputStream bytes = new ByteArrayOutputStream();
-        bmp.compress(Bitmap.CompressFormat.JPEG, 60, bytes);
+
+        bmp = cropBitmap( bmp, 400, 400, DECODE_FLAG_CROPFIT );
+
+        bmp.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
 
         String file = null;
         if(callerActivity.equals("M2A")) {
